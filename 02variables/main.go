6137c7a630e723abc2061cf34b4438b214e0417e
Tejas-pr <Tejas.teju02@gmail.com>
2025-08-05 20:37:38 +0530

    proj 1 and 2

---
+++ 02variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var hello int = 33333
+
+func main() {
+	var username string = "Tejas"
+	fmt.Println("Hello, " + username + "!")
+
+	myNameIs := "Tejas"
+	fmt.Println("My name is " + myNameIs + ".")
+	fmt.Println("My number is", hello)
+}
