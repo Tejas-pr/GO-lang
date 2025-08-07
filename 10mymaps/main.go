73804c1d9592e7ded6ea40dbd07179144fd65422
Tejas <tejas.teju02@gmail.com>
2025-08-07 19:24:53 +0530

    video 16 done

---
+++ 10mymaps/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// how to make map in go
+	language := make(map[string]string)
+
+	language["JS"] = "Javascript"
+	language["RB"] = "Ruby"
+	language["PY"] = "Python"
+
+	fmt.Println("List of all languages : ", language)
+	fmt.Println("List of all languages : ", language["JS"])
+
+	delete(language, "RB")
+	fmt.Println("List of all languages : ", language)
+
+	// loops
+	for key, value := range language {
+		fmt.Printf("the key is %v and the value is %v\n", key, value)
+	}
+
+	for _, value := range language {
+		fmt.Printf("the value is %v\n", value)
+	}
+}
