16bf8c66adc59ac536cab1d39ffa3612f377ed52
Tejas-pr <Tejas.teju02@gmail.com>
2025-08-09 16:35:05 +0530

    practice map

--- 10mymaps/main.go
+++ 10mymaps/main.go
@@ -24,4 +24,14 @@ func main() {
 	for _, value := range language {
 		fmt.Printf("the value is %v\n", value)
 	}
+
+	language2 := make(map[string]string)
+
+	language2["TJ"] = "tejas"
+	language2["TJP"] = "tejasPR"
+
+	for key, value := range language2 {
+		fmt.Println("the new language two is : ======================")
+		fmt.Printf("the key is %v and the value is %v\n", key, value)
+	}
 }
