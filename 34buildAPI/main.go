9466069f61678f3c4facdb84d8ebaaf538bae744
Tejas-pr <Tejas.teju02@gmail.com>
2025-09-04 19:50:44 +0530

    completed DB

--- 34buildAPI/main.go
+++ 34buildAPI/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -33,7 +34,37 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
-	// serveHome()
+	r := mux.NewRouter()
+	// seeding
+	courses = append(courses, Course{
+		CourseId:   "1",
+		CourseName: "ReactJS",
+		Price:      299,
+		Author: &Author{
+			Fullname: "Tejas",
+			Website:  "tejas.dev",
+		},
+	})
+	courses = append(courses, Course{
+		CourseId:   "2",
+		CourseName: "MERN",
+		Price:      199,
+		Author: &Author{
+			Fullname: "Chai",
+			Website:  "Chai.dev",
+		},
+	})
+
+	// routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controllers - file
@@ -64,7 +95,6 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode("No Course id !!!")
-	return
 }
 
 // to create a course
@@ -78,7 +108,15 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var course Course
-	_ = json.NewDecoder(r.Body).Decode(&courses)
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	for _, courseIndex := range courses {
+		if courseIndex.CourseName == course.CourseName {
+			json.NewEncoder(w).Encode("this course already exists!!")
+			return
+		}
+	}
+
 	if course.IsEmpty() {
 		json.NewEncoder(w).Encode("No data !!")
 		return
@@ -90,7 +128,6 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	course.CourseId = strconv.Itoa(rand.Intn(100))
 	courses = append(courses, course)
 	json.NewEncoder(w).Encode(course)
-	return
 }
 
 // update
@@ -113,4 +150,25 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode("course id not found !!!")
 		}
 	}
+}
+
+// delete
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("delete one")
+	w.Header().Set("Content-type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(map[string]string{"message": "successfully deleted"})
+			return
+		}
+	}
+
+	// If no match found
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{"error": "Id not found"})
 }
